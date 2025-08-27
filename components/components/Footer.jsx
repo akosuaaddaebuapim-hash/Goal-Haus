cb9758003c84c6ebb7d8e851b267c97bb56de0f6
Akosua Addae Buapim <akosuaaddaebuapim@gmail.com>
2025-08-27 02:00:21 +0000

    Create Footer.jsx

---
+++ components/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-900 text-center p-4 mt-6">
+      <p className="text-gray-400 text-sm">
+        © {new Date().getFullYear()} Goal-Haus | Your Haus of Football. Live & Free.
+      </p>
+    </footer>
+  );
+}
